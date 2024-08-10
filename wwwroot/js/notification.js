fbec8ae387b746da92a714d42d55a00270daef11
Lester Ai <lesterai001@gmail.com>
2024-08-10 15:38:47 +0800

    section 12: category CRUD Add Toastr to Project

---
+++ wwwroot/js/notification.js
@@ -0,0 +1,8 @@
+﻿Window.ShowToastr = function (type, message) {
+    if (type == "success") {
+        toastr.success(message);
+    }
+    if (type == "error") {
+        toastr.error(message);
+    }
+}
